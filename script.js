f56cecd5ac8857e63ba56aecafd3546b5967d759
mdaminuddinshah <muhammadaminuddinshah98@gmail.com>
2024-03-05 00:25:38 +0800

    add delete button

--- script.js
+++ script.js
@@ -6,14 +6,46 @@ const listsContainer = document.getElementById('lists');
 const cls = document.getElementById('cls');
 
 btns.addEventListener('click', function(){
+
+    // ni untuk div element, so boleh masukkan button create dan delete dlm div ni
+    let createDiv = document.createElement('div');
+    createDiv.style.display = 'flex';
+
+    // ni utk button create para dan delete button
     let createParagraph = document.createElement('button');
+    let deleteParagraph = document.createElement('button');
+
     createParagraph.textContent = inputlist.value;
+    deleteParagraph.textContent = 'Delete';
+
+    createParagraph.style.backgroundColor = 'rgba(0, 120, 255, 1)';
+    deleteParagraph.style.backgroundColor = 'rgba(255, 0, 45)';
+
+
+    createParagraph.style.width = '200px';
+    deleteParagraph.style.width = '100px';
+
     createParagraph.style.display = 'inline-block';
+    deleteParagraph.style.display = 'inline-block';
+
+    createParagraph.style.color = 'white';
+    deleteParagraph.style.color = 'white';
+
+    createParagraph.style.marginBottom = '3px';
+    deleteParagraph.style.marginBottom = '3px';
+
     createParagraph.style.marginRight = '3px';
+    deleteParagraph.style.marginLeft = '3px';
+
     createParagraph.style.border = '1px solid black';
     createParagraph.style.borderRadius = '7px';
 
-    listsContainer.appendChild(createParagraph);
+    deleteParagraph.style.border = '1px solid black';
+    deleteParagraph.style.borderRadius = '7px';
+
+    createDiv.appendChild(createParagraph);
+    createDiv.appendChild(deleteParagraph);
+    listsContainer.appendChild(createDiv);
     
 })
 
