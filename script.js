ce4ec822d66d080be26704d188d2afd716f4c51b
mdaminuddinshah <muhammadaminuddinshah98@gmail.com>
2024-03-05 00:45:22 +0800

    add edit button

--- script.js
+++ script.js
@@ -14,41 +14,54 @@ btns.addEventListener('click', function(){
     // ni utk button ( create para dan delete button )
     let createParagraph = document.createElement('button');
     let deleteParagraph = document.createElement('button');
+    let editParagraph = document.createElement('button');
 
     createParagraph.textContent = inputlist.value;
     deleteParagraph.textContent = 'Delete';
+    editParagraph.textContent = "Edit"
 
-    createParagraph.style.backgroundColor = 'rgba(0, 120, 255, 1)';
+    createParagraph.style.backgroundColor = 'white';
     deleteParagraph.style.backgroundColor = 'rgba(255, 0, 45)';
+    editParagraph.style.backgroundColor = 'rgba(0, 206, 87, 0.8)';
 
     createParagraph.style.width = '200px';
     deleteParagraph.style.width = '100px';
+    editParagraph.style.width = '100px';
 
     createParagraph.style.display = 'inline-block';
     deleteParagraph.style.display = 'inline-block';
+    editParagraph.style.display = 'inline-block';
 
-    createParagraph.style.color = 'white';
+    createParagraph.style.color = 'dark';
     deleteParagraph.style.color = 'white';
+    editParagraph.style.color = 'white';
 
     createParagraph.style.marginBottom = '3px';
     deleteParagraph.style.marginBottom = '3px';
+    editParagraph.style.marginBottom = '3px';
 
     createParagraph.style.marginRight = '3px';
     deleteParagraph.style.marginLeft = '3px';
+    editParagraph.style.marginLeft = '5px';
 
     createParagraph.style.border = '1px solid black';
     createParagraph.style.borderRadius = '7px';
+    editParagraph.style.border = '1px solid black';
 
     deleteParagraph.style.border = '1px solid black';
     deleteParagraph.style.borderRadius = '7px';
+    editParagraph.style.borderRadius = '7px';
 
     createDiv.appendChild(createParagraph);
     createDiv.appendChild(deleteParagraph);
+    createDiv.appendChild(editParagraph);
+
     listsContainer.appendChild(createDiv);
     
     deleteParagraph.addEventListener('click', function(){
         createDiv.removeChild(createParagraph)
         createDiv.removeChild(deleteParagraph)
+        createDiv.removeChild(editParagraph)
     })
 })
 
